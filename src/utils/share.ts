1dabe237c3b5eb270083d07c9d0d55af2dd668ea
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-12-31 03:13:56 +0900

    feat: add og:image for facebook

--- src/utils/share.ts
+++ src/utils/share.ts
@@ -11,7 +11,7 @@ export const shareWithKakaotalk = (shareLink: string, shareName: string): void =
     content: {
       title: `${shareName} 님께 새로운 감사장이 도착했어요!`,
       description: '🎉 아래 버튼을 눌러서 확인해 주세요.',
-      imageUrl: 'https://raw.githubusercontent.com/junhoyeo/citation/master/src/assets/images/kakao.png',
+      imageUrl: 'https://raw.githubusercontent.com/junhoyeo/citation/master/src/assets/images/kakaotalk.png',
       link: {
         mobileWebUrl: shareLink,
         webUrl: shareLink,
