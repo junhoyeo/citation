49a59b14483e02f6e30be9fc55ac8a11307b7662
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-12-30 19:02:13 +0900

    feat: add illust in default Home

--- src/pages/Home/HomeForDefault.tsx
+++ src/pages/Home/HomeForDefault.tsx
@@ -1,12 +1,17 @@
 import * as React from 'react';
+import styled from 'styled-components';
 import { History } from 'history';
 
+import Button from '../../components/atoms/Button';
+import Header from '../../components/atoms/Header';
+import Illust from '../../components/atoms/Illust';
 import {
   Text,
   TextForTitle,
 } from '../../components/atoms/Text';
-import Button from '../../components/atoms/Button';
 import CardList from '../../components/templates/CardList';
+
+import partyIllust from '../../assets/illusts/party.png';
 
 export type HomeForDefaultProps = {
   history: History,
@@ -26,25 +31,67 @@ export const HomeForDefault: React.FC<HomeForDefaultProps> = ({ history }) => {
 
   return (
     <>
-      <TextForTitle>
-        지금까지 총 <Text>0</Text>장의 감사장을 보내셨네요!
-      </TextForTitle>
+      <RelativeHeader>
+        <TextForTitle>
+          지금까지<br />
+          총 <Text>2,100</Text>장의<br />
+          감사장을<br />
+          보내셨네요!
+        </TextForTitle>
+        <AbsoluteIllust
+          src={partyIllust}
+        />
+      </RelativeHeader>
       <CardList
         cards={[]}
       />
-      <Button
-        onClick={() => history.push('/create')}
-      >
-        감사장 선물하기
-      </Button>
-      <Button
-        onClick={onClickReset}
-        primary={false}
-      >
-        초기화
-      </Button>
+      <ButtonList>
+        <Button
+          onClick={() => history.push('/create')}
+        >
+          감사장 선물하기
+        </Button>
+        <Button
+          onClick={onClickReset}
+          primary={false}
+        >
+          초기화
+        </Button>
+      </ButtonList>
     </>
   );
 };
 
 export default HomeForDefault;
+
+const RelativeHeader = styled(Header)`
+  position: relative;
+
+  @media (max-width: 500px) {
+    position: unset;
+  }
+`;
+
+const AbsoluteIllust = styled(Illust)`
+  position: absolute;
+  right: 0;
+  top: 0;
+  z-index: -1;
+  margin: 0;
+
+  @media (max-width: 500px) {
+    top: 20px;
+    right: -60px;
+  }
+`;
+
+const ButtonList = styled.div`
+  position: fixed;
+  bottom: 0;
+  width: 400px;
+  margin-bottom: 1.5rem;
+
+  @media (max-width: 500px) {
+    width: 80%;
+  }
+`;
